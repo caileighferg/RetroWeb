55f2f334a34750c6ae46f3c2bacc77a10083159f
caileighferg <caileigh.n.ferguson-1@ou.edu>
2022-10-19 11:53:37 -0500

    setup
    added files to new repository

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(".infoBox1").hide();
+
+$("#year1").click(function(){
+    $(".infoBox1").show();
+});
+
+$(".ex1").click(function(){
+    $(".infoBox1").hide();    
+});  
+    
+    
+$(".infoBox2").hide();
+
+$("#year2").click(function(){
+    $(".infoBox2").show();
+});
+
+$(".ex2").click(function(){
+    $(".infoBox2").hide();
+});
+
+
+$(".infoBox3").hide();
+
+$("#year3").click(function(){
+    $(".infoBox3").show();
+});
+
+$(".ex3").click(function(){
+    $(".infoBox3").hide();
+});
+
+
+$(".infoBox4").hide();
+
+$("#year4").click(function(){
+    $(".infoBox4").show();
+});
+
+$(".ex4").click(function(){
+    $(".infoBox4").hide();
+});

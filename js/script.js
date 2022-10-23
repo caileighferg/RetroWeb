690064b8a5bbdf2541ac54e3c671ea6adccd179f
caileighferg <caileigh.n.ferguson-1@ou.edu>
2022-10-23 13:54:41 -0500

    updates
    updated code

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ $(".infoBox1").hide();
 
 $("#year1").click(function(){
     $(".infoBox1").show();
+    $("#timeline, #year, #picture, #footer").addClass("containerOp");
 });
 
 $(".ex1").click(function(){
-    $(".infoBox1").hide();    
+    $(".infoBox1").hide();
+    $("#timeline, #year, #picture, #footer").removeClass("containerOp");
 });  
     
     
@@ -13,10 +15,12 @@ $(".infoBox2").hide();
 
 $("#year2").click(function(){
     $(".infoBox2").show();
+    $("#timeline, #year, #picture, #footer").addClass("containerOp");
 });
 
 $(".ex2").click(function(){
     $(".infoBox2").hide();
+    $("#timeline, #year, #picture, #footer").removeClass("containerOp");
 });
 
 
@@ -24,10 +28,12 @@ $(".infoBox3").hide();
 
 $("#year3").click(function(){
     $(".infoBox3").show();
+    $("#timeline, #year, #picture, #footer").addClass("containerOp");
 });
 
 $(".ex3").click(function(){
     $(".infoBox3").hide();
+    $("#timeline, #year, #picture, #footer").removeClass("containerOp");
 });
 
 
@@ -35,8 +41,10 @@ $(".infoBox4").hide();
 
 $("#year4").click(function(){
     $(".infoBox4").show();
+    $("#timeline, #year, #picture, #footer").addClass("containerOp");
 });
 
 $(".ex4").click(function(){
     $(".infoBox4").hide();
+    $("#timeline, #year, #picture, #footer").removeClass("containerOp");
 });
